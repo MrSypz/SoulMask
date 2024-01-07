df93943239560c8e58953e746a163dea495a846c
MrSypz <chainot@hotmail.co.th>
2024-01-07 15:51:20 +0700

    Vizard CCA init

--- src/main/java/sypztep/mamy/soulmask/common/init/ModEntityComponents.java
+++ src/main/java/sypztep/mamy/soulmask/common/init/ModEntityComponents.java
@@ -4,6 +4,8 @@ import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
+import net.minecraft.entity.player.PlayerEntity;
 import sypztep.mamy.soulmask.common.SoulMaskMod;
 import sypztep.mamy.soulmask.common.component.VizardComponent;
 
@@ -11,6 +13,6 @@ public class ModEntityComponents implements EntityComponentInitializer {
     public static final ComponentKey<VizardComponent> VIZARD = ComponentRegistry.getOrCreate(SoulMaskMod.id("vizard"), VizardComponent.class);
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
-
+        registry.beginRegistration(PlayerEntity.class, VIZARD).respawnStrategy(RespawnCopyStrategy.CHARACTER).end(VizardComponent::new);
     }
 }
