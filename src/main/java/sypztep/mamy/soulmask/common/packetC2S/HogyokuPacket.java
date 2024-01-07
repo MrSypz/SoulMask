7f22e926f955599315ed0ac7da229eae8ad4a7c3
MrSypz <chainot@hotmail.co.th>
2024-01-07 15:51:28 +0700

    Network

---
+++ src/main/java/sypztep/mamy/soulmask/common/packetC2S/HogyokuPacket.java
@@ -0,0 +1,27 @@
+package sypztep.mamy.soulmask.common.packetC2S;
+
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import sypztep.mamy.soulmask.common.SoulMaskMod;
+import sypztep.mamy.soulmask.common.component.VizardComponent;
+
+public class HogyokuPacket {
+    public static final Identifier ID = SoulMaskMod.id("hogyoku");
+    public static void send() {
+        ClientPlayNetworking.send(ID, new PacketByteBuf(Unpooled.buffer()));
+    }
+    //
+    public static class Receiver implements ServerPlayNetworking.PlayChannelHandler {
+        @Override
+        public void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+            VizardComponent.incHogyoku(player);
+        }
+    }
+}
