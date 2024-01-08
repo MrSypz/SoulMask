3567edfc08310b9bee7b23fecf8a3b4eae2ddd68
MrSypz <chainot@hotmail.co.th>
2024-01-08 19:50:30 +0700

    Cooldown Function

--- src/main/java/sypztep/mamy/soulmask/client/event/MaskHandleTick.java
+++ src/main/java/sypztep/mamy/soulmask/client/event/MaskHandleTick.java
@@ -4,33 +4,30 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import sypztep.mamy.soulmask.client.SoulMaskModClient;
 import sypztep.mamy.soulmask.common.component.VizardComponent;
 import sypztep.mamy.soulmask.common.packetC2S.MaskPacket;
-import sypztep.mamy.soulmask.common.packetC2S.UnMaskPacket;
 import sypztep.mamy.soulmask.common.util.SoulMaskUtil;
 
 public class MaskHandleTick {
     private static final int DEFAULT_COOLDOWN = 20;
-    private static int equipmaskcd = DEFAULT_COOLDOWN,lastequipcd = DEFAULT_COOLDOWN, unmaskcd = DEFAULT_COOLDOWN,lastunmaskcd = DEFAULT_COOLDOWN;
+    private static int equipmaskcd = DEFAULT_COOLDOWN, unmaskcd = DEFAULT_COOLDOWN;
+    private static boolean wasUnEquipMask = false;
 
     public static void init () {
         ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
             if (minecraft.player != null) {
-                if (!SoulMaskUtil.hasEquippedMask(minecraft.player) && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed()) {
-                    if (VizardComponent.canUseMask(minecraft.player)) {
-                        equipmaskcd--;
-                        if (equipmaskcd <= 0) {
-                            MaskPacket.send();
-                            equipmaskcd = DEFAULT_COOLDOWN;
-                        }
+                if (!SoulMaskUtil.hasEquippedMask(minecraft.player) && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed() && VizardComponent.canUseMask(minecraft.player) && !VizardComponent.WasEquipMask(minecraft.player)) {
+                    equipmaskcd--;
+                    if (equipmaskcd <= 0) {
+                        MaskPacket.send(1);
+                        equipmaskcd = DEFAULT_COOLDOWN;
                     }
-                }
-                else if (SoulMaskUtil.hasEquippedMask(minecraft.player) && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed()) {
+                } else if (SoulMaskUtil.hasEquippedMask(minecraft.player) && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed()) {
                     unmaskcd--;
                     if (unmaskcd <= 0) {
-                        UnMaskPacket.send();
+                        MaskPacket.send(2);
+                        wasUnEquipMask = true;
                         unmaskcd = DEFAULT_COOLDOWN;
                     }
-                }
-                else {
+                } else {
                     unmaskcd = DEFAULT_COOLDOWN;
                     equipmaskcd = DEFAULT_COOLDOWN;
                 }
@@ -44,12 +41,18 @@ public class MaskHandleTick {
     public static int getUnmaskcd() {
         return unmaskcd;
     }
-
     public static int getLastequipcd() {
-        return lastequipcd;
+        return DEFAULT_COOLDOWN;
+    }
+    public static int getLastunmaskcd() {
+        return DEFAULT_COOLDOWN;
     }
 
-    public static int getLastunmaskcd() {
-        return lastunmaskcd;
+    public static boolean isWasUnEquipMask() {
+        return wasUnEquipMask;
+    }
+
+    public static void setWasUnEquipMask(boolean wasUnEquipMask) {
+        MaskHandleTick.wasUnEquipMask = wasUnEquipMask;
     }
 }
