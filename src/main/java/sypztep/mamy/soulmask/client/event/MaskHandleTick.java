c066a8a49821925f31e98a2c178e268900a8de44
Sypztep <chainot@hotmail.co.th>
2024-03-27 18:05:43 +0700

    Clean up

--- src/main/java/sypztep/mamy/soulmask/client/event/MaskHandleTick.java
+++ src/main/java/sypztep/mamy/soulmask/client/event/MaskHandleTick.java
@@ -1,6 +1,7 @@
 package sypztep.mamy.soulmask.client.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import sypztep.mamy.soulmask.client.SoulMaskModClient;
 import sypztep.mamy.soulmask.common.component.VizardComponent;
 import sypztep.mamy.soulmask.common.packetC2S.MaskPacket;
@@ -8,69 +9,81 @@ import sypztep.mamy.soulmask.common.utils.SoulMaskUtil;
 
 public class MaskHandleTick {
     private static final int DEFAULT_COOLDOWN = 20;
-    private static int equipmaskcd = DEFAULT_COOLDOWN, unmaskcd = DEFAULT_COOLDOWN,presscd = 10;
+    private static int equipmaskcd = DEFAULT_COOLDOWN, unmaskCooldown = DEFAULT_COOLDOWN, pressCooldown = 10;
     private static boolean wasUnEquipMask = false, hasEquippedMask = false;
 
-    public static void init () {
+    public static void init() {
         ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
             if (minecraft.player != null) {
                 hasEquippedMask = SoulMaskUtil.hasEquippedMask(minecraft.player);
-                if (!hasEquippedMask && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed() && VizardComponent.canUseMask(minecraft.player) && !VizardComponent.WasEquipMask(minecraft.player) && presscd <= 0) {
 
-                    if (minecraft.player.age % 5 == 0) {
-                        SoulMaskUtil.addChargeParticle(minecraft.player);
-                        MaskPacket.send(3);
-                    }
-                    equipmaskcd--;
+//                if (SoulMaskModClient.DEBUG_KEYBINDING.isPressed())
 
-                    if (equipmaskcd <= 0) {
-                        presscd = 10;
-                        MaskPacket.send(1);
-                        SoulMaskUtil.addUseMaskParticle(minecraft.player);
-                        equipmaskcd = DEFAULT_COOLDOWN;
-                    }
-                } else if (hasEquippedMask && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed() && presscd <= 0) {
-
-                    if (minecraft.player.age % 5 == 0) {
-                        SoulMaskUtil.addChargeParticle(minecraft.player);
-                        MaskPacket.send(3);
-                    }
-                    unmaskcd--;
-                    if (unmaskcd <= 0) {
-                        presscd = 10;
-                        MaskPacket.send(2);
-                        wasUnEquipMask = true;
-                        SoulMaskUtil.addUseMaskParticle(minecraft.player);
-                        unmaskcd = DEFAULT_COOLDOWN;
-                    }
+                if (!hasEquippedMask && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed() && VizardComponent.canUseMask(minecraft.player) && !wasUnEquipMask && pressCooldown <= 0) {
+                    handleMaskEquipping(minecraft);
+                } else if (hasEquippedMask && SoulMaskModClient.EQUIPMASK_KEYBINDING.isPressed() && pressCooldown <= 0) {
+                    handleMaskUnequipping(minecraft);
                 } else {
-                    unmaskcd = DEFAULT_COOLDOWN;
-                    equipmaskcd = DEFAULT_COOLDOWN;
+                    resetCooldowns();
                 }
-                if (presscd > 0)
-                    presscd--;
+                if (pressCooldown > 0)
+                    pressCooldown--;
             }
         });
     }
-    /**
-     * Render UI part
-     */
+
+    private static void handleMaskEquipping(MinecraftClient minecraft) {
+        if (minecraft.player.age % 5 == 0) {
+            SoulMaskUtil.addChargeParticle(minecraft.player);
+            MaskPacket.send(3);
+        }
+        equipmaskcd--;
+
+        if (equipmaskcd <= 0) {
+            pressCooldown = 10;
+            MaskPacket.send(1);
+            SoulMaskUtil.addUseMaskParticle(minecraft.player);
+            equipmaskcd = DEFAULT_COOLDOWN;
+        }
+    }
+
+    private static void handleMaskUnequipping(MinecraftClient minecraft) {
+        if (minecraft.player.age % 5 == 0) {
+            SoulMaskUtil.addChargeParticle(minecraft.player);
+            MaskPacket.send(3);
+        }
+        unmaskCooldown--;
+        if (unmaskCooldown <= 0) {
+            pressCooldown = 10;
+            MaskPacket.send(2);
+            wasUnEquipMask = true;
+            SoulMaskUtil.addUseMaskParticle(minecraft.player);
+            unmaskCooldown = DEFAULT_COOLDOWN;
+        }
+    }
+
+    private static void resetCooldowns() {
+        unmaskCooldown = DEFAULT_COOLDOWN;
+        equipmaskcd = DEFAULT_COOLDOWN;
+    }
+
+    // UI rendering part
     public static int getEquipmaskcd() {
         return equipmaskcd;
     }
-    public static int getUnmaskcd() {
-        return unmaskcd;
+
+    public static int getUnmaskCooldown() {
+        return unmaskCooldown;
     }
+
     public static int getLastequipcd() {
         return DEFAULT_COOLDOWN;
     }
+
     public static int getLastunmaskcd() {
         return DEFAULT_COOLDOWN;
     }
-
-    /**
-     * End Render UI part
-     */
+    // End UI rendering part
 
     public static boolean WasUnEquipMask() {
         return wasUnEquipMask;
