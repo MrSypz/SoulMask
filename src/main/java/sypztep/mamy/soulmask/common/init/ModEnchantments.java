c3e749a931b51b0ce01668cd2f55987513ab9d48
Sypztep <chainot@hotmail.co.th>
2024-03-28 14:35:58 +0700

    Add Enchantment

--- src/main/java/sypztep/mamy/soulmask/common/init/ModEnchantments.java
+++ src/main/java/sypztep/mamy/soulmask/common/init/ModEnchantments.java
@@ -5,15 +5,11 @@ import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import sypztep.mamy.soulmask.common.SoulMaskMod;
 import sypztep.mamy.soulmask.common.enchantment.HollowCurseEnchantment;
 
 public class ModEnchantments {
     public static Enchantment HOLLOW_CURSE = new HollowCurseEnchantment(Enchantment.Rarity.RARE, EnchantmentTarget.ARMOR, EquipmentSlot.HEAD);
-    public static void register() {
-        register("hollowcurse",HOLLOW_CURSE);
-    }
-    public static void register(String name, Enchantment enchantment){
-        Registry.register(Registries.ENCHANTMENT, SoulMaskMod.id(name), enchantment);
+    public static void init() {
+        Registry.register(Registries.ENCHANTMENT,"hollowcurse",HOLLOW_CURSE);
     }
 }
