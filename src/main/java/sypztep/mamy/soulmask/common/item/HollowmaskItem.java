c8b29a5a615e0764cdda7adb00299fa186eb4763
Sypztep <chainot@hotmail.co.th>
2024-03-28 14:36:17 +0700

    Add Fucntion to maskitem

--- src/main/java/sypztep/mamy/soulmask/common/item/HollowmaskItem.java
+++ src/main/java/sypztep/mamy/soulmask/common/item/HollowmaskItem.java
@@ -8,10 +8,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorMaterials;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
+import sypztep.mamy.soulmask.common.component.VizardComponent;
 import sypztep.mamy.soulmask.common.init.ModItems;
 import sypztep.mamy.soulmask.common.init.ModSoundEvents;
+
+import static sypztep.mamy.soulmask.common.init.ModStatusEffects.HOLLOW_POWER;
 
 public class HollowmaskItem extends SoulMaskFuncItem {
 
@@ -32,27 +34,30 @@ public class HollowmaskItem extends SoulMaskFuncItem {
         return EquipmentSlot.HEAD;
     }
 
-//    @Override
-//    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-//        if (entity instanceof PlayerEntity player) {
-//            ItemStack headSlot = player.getEquippedStack(EquipmentSlot.HEAD);
-//            boolean hasPowerHollow = player.hasStatusEffect(HOLLOW_POWER);
-//            if (headSlot.isOf(this)) {
-//                if (!hasPowerHollow) {
-//                    int amp = headSlot.isOf(ModItems.VASTO_MASK) ? 4 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER4) ? 3 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER3) ? 2 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER2) ? 1 : 0;
-//                    int amp2 = headSlot.isOf(ModItems.VASTO_MASK) ? 4 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER4) ? 3 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER3) ? 2 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER2) ? 1 : 0;
-//                    player.addStatusEffect(new StatusEffectInstance(HOLLOW_POWER, 100, amp2, false, false, false));
-//                    if (!headSlot.isOf(ModItems.HALF_HOLLOW_MASK))
-//                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, amp, false, false, false));
-//                    if (player.getHungerManager().getFoodLevel() <= 0)
-//                        player.addStatusEffect(new StatusEffectInstance(STIMULATION, 100, 0, false, false,false));
-//                }
-//            }
-//            if (player.getAttributes().getBaseValue(ModEntityAttributes.GENERIC_HOGYOKU) < 1 && headSlot.isOf(this)) {
-//                player.equipStack(EquipmentSlot.HEAD,new ItemStack(ItemStack.EMPTY.getItem()));
-//                player.damage(player.getWorld().getDamageSources().create(ModDamageTypes.MASKIMPACT), + player.getHealth() * 0.99f);
-//            }
-//        }
-//        super.inventoryTick(stack, world, entity, slot, selected);
-//    }
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (entity instanceof PlayerEntity player) {
+            ItemStack headSlot = player.getEquippedStack(EquipmentSlot.HEAD);
+            boolean hasPowerHollow = player.hasStatusEffect(HOLLOW_POWER);
+            if (headSlot.isOf(this)) {
+                if (!hasPowerHollow) {
+                    int amp = headSlot.isOf(ModItems.VASTO_MASK) ? 4 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER4) ? 3 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER3) ? 2 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER2) ? 1 : 0;
+                    int amp2 = headSlot.isOf(ModItems.VASTO_MASK) ? 4 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER4) ? 3 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER3) ? 2 : headSlot.isOf(ModItems.HOLLOW_MASK_TIER2) ? 1 : 0;
+                    player.addStatusEffect(new StatusEffectInstance(HOLLOW_POWER, 100, amp2, false, false, false));
+                    if (!headSlot.isOf(ModItems.HALF_HOLLOW_MASK))
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, amp, false, false, false));
+                }
+            }
+            if (VizardComponent.getHogyokuValue(player) < 1 && headSlot.isOf(this)) {
+                player.equipStack(EquipmentSlot.HEAD, new ItemStack(ItemStack.EMPTY.getItem()));
+                player.damage(player.getWorld().getDamageSources().playerAttack(player),player.getHealth() * 0.5f);
+            }
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    @Override
+    public boolean isDamageable() {
+        return false;
+    }
 }
