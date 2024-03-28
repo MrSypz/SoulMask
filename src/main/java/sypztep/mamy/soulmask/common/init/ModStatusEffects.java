3a8a5a2517ad5e8bc0f51e2840f21a86369d0ace
Sypztep <chainot@hotmail.co.th>
2024-03-28 14:40:38 +0700

    tooltip and item function

--- src/main/java/sypztep/mamy/soulmask/common/init/ModStatusEffects.java
+++ src/main/java/sypztep/mamy/soulmask/common/init/ModStatusEffects.java
@@ -12,6 +12,7 @@ import sypztep.mamy.soulmask.common.statuseffects.EmptyStatusEffect;
 public class ModStatusEffects {
     public static final StatusEffect HOLLOW_POWER = new EmptyStatusEffect(StatusEffectCategory.BENEFICIAL,0)
             .addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE,"52f38a34-b1a7-4a65-928e-8a30608b7432",0.12D, EntityAttributeModifier.Operation.MULTIPLY_BASE)
+            .addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH,"52f38a34-b1a7-4a65-928e-8a30608b7432",0.2D, EntityAttributeModifier.Operation.MULTIPLY_BASE)
             .addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED,"90c7a8a9-2b36-43ce-a28d-08d29c1ebead",0.02D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
     public static void init() {
         Registry.register(Registries.STATUS_EFFECT, SoulMaskMod.id("hollowpower"), HOLLOW_POWER);
